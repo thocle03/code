db706858aab5e11b6cbd010081f857d548102558
thocle03 <thocle03@gmail.com>
2024-01-23 14:57:45 +0100

    coucou

--- boutique.js
+++ boutique.js
@@ -2,8 +2,12 @@ let product = ["banane", "aspirateur", "zebre", "telephone", "bouteille"];
 let price = [4, 8, 6, 898, 5];
 let money = prompt("Combien d'argent vous avez ?");
 let panier = [];
+var xhr = new XMLHttpRequest();
+let want = "USD"
+let have = "EUR"
+
 if (money != 0) {
-    console.log("vous avez injecté " + money + "$");
+    console.log("vous avez injecté " + money + "€");
 }
 else {
     console.log("Veillez mettre de l'argent afin de pouvoir effectuer des achats et acceder a votre panier");
@@ -13,7 +17,19 @@ do {
     if (consulting == "oui") {
         let consultSold = prompt("voulez vous consulter votre solde");
         if (consultSold == "oui") {
-            console.log(money);
+            console.log(money + " €");
+            want = prompt("Quelle type de monnaie?")
+            xhr.open('GET', `https://api.api-ninjas.com/v1/convertcurrency?want=€{want}&have=€{have}&amount=€{money}`, true);
+            xhr.setRequestHeader('X-Api-Key', 'gFNcacKBJnQYcfqC2tiA0g==s71N5tTWrsyz1oUd');
+            xhr.setRequestHeader('Content-Type', 'application/json');
+            xhr.onload = function () {
+                if (xhr.status >= 200 && xhr.status < 300) {
+                    console.log(JSON.parse(xhr.responseText));
+                } else {
+                    console.error('Error: ', xhr.responseText);
+                }
+            };
+            xhr.send();
         }
         let injectSold = prompt("voulez vous remettre de l'argent ?");
         if (injectSold == "oui") {
@@ -40,7 +56,7 @@ do {
 function startShopping() {
 
     for (let i = 0; i < product.length; i++) {
-        console.log("Prduit n°" + (i + 1) + " " + product[i] + " " + price[i] + "$");
+        console.log("Prduit n°" + (i + 1) + " " + product[i] + " " + price[i] + "€");
     }
     if (money > 0) {
         let ajoutArticle = prompt("Voulez vous ajouter un article au panier ?")
@@ -65,9 +81,9 @@ function addProduct() {
     console.log("Votre panier");
     if (product[choiceProduct - 1] != "") {
         for (let numeroProduit = 0; numeroProduit < panier.length; numeroProduit++) {
-            console.log("Produit n°" + (panier[numeroProduit] + 1) + " " + (product[panier[numeroProduit]]) + " " + price[panier[numeroProduit]] + " $");
+            console.log("Produit n°" + (panier[numeroProduit] + 1) + " " + (product[panier[numeroProduit]]) + " " + price[panier[numeroProduit]] + " €");
         }
-        console.log("votre solde s'élève à " + money + "$");
+        console.log("votre solde s'élève à " + money + "€");
     }
     ajoutArticle = prompt("Voulez vous encore ajouter un article au panier ?")
     if (ajoutArticle == "oui") {
@@ -80,9 +96,9 @@ function addProduct() {
             for (let numeroPrix = 0; numeroPrix < panier.length; numeroPrix++) {
                 total += price[panier[numeroPrix]];
             }
-            console.log("Votre panier coûte au total : " + total + "$");
+            console.log("Votre panier coûte au total : " + total + "€");
             money -= total;
-            console.log("il vous reste :" + money + "$");
+            console.log("il vous reste :" + money + "€");
             if (money > 0) {
                 startShopping();
             }
